6ea5ab6ae696881b9e82a4a6794f1ec3df6ac807
Zhongkai <yezhongkai@outlook.com>
2020-03-03 14:21:25 -0500

    Project Backend Init

---
+++ Backend/MSIRoomBooking/src/main/java/com/mecury/MSIRoomBooking/MsiRoomBookingApplication.java
@@ -0,0 +1,13 @@
+package com.mecury.MSIRoomBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsiRoomBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsiRoomBookingApplication.class, args);
+	}
+
+}
---
+++ Backend/MSIRoomBooking/src/test/java/com/mecury/MSIRoomBooking/MsiRoomBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mecury.MSIRoomBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsiRoomBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
